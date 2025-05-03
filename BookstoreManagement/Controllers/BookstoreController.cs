ce48cf16256fa138aba0a426a8d7506a952a4741
Vitor Hugo <vv001226@gmail.com>
2025-05-03 13:04:26 -0300

    Iniciando projeto

--- BookstoreManagement/Controllers/BookstoreController.cs
+++ BookstoreManagement/Controllers/BookstoreController.cs
@@ -11,4 +11,28 @@ public class BookstoreController : ControllerBase
     {
         return Ok("Hello World!");
     }
+    
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        return Ok($"Hello World! {id}");
+    }
+    
+    [HttpPost]
+    public IActionResult Post([FromBody] string value)
+    {
+        return Ok($"Hello World! {value}");
+    }
+    
+    [HttpPut("{id}")]
+    public IActionResult Put(int id, [FromBody] string value)
+    {
+        return Ok($"Hello World! {id} {value}");
+    }
+    
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        return Ok($"Hello World! {id}");
+    }
 }
