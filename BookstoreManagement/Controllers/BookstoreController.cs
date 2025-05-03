c32c7d7c57d0441d1510db148ff5f00be1aac4b7
Vitor Hugo <vv001226@gmail.com>
2025-05-03 12:58:50 -0300

    first commit

---
+++ BookstoreManagement/Controllers/BookstoreController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookstoreManagement.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BookstoreController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Hello World!");
+    }
+}
